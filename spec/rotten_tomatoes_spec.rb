d0c2c4561f7412108537bbd1c2d8f0c8ae5099d0
Cernan Bernardo <github email address>
2018-11-08 06:57:36 -0800

    Initial commit

---
+++ spec/rotten_tomatoes_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe RottenTomatoes do
+  it "has a version number" do
+    expect(RottenTomatoes::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
